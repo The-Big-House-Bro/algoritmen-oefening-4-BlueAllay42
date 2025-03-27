4f256f8eae07b9307d31b6ce64a0384411a4d3fb
Linus Bailleul <BlueAllay42@gmail.com>
2025-03-27 19:47:32 +0100

    taak morgen

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,12 @@
 def countTargetPairs():
+     nums.sort()
+        juiste = 0 
+        linksegetallen = 0
+        rechtsegetallen = len(nums)-1
+        while linksegetallen < rechtsegetallen: #zolang het linker getal kleiner is dan het rechter
+            if nums[linksegetallen] + nums[rechtsegetallen] < target: #de coordinaten van de getallen moeten kleiner zijn dan het target
+                juist = juist + rechtsegetallen - linksegetallen 
+                linksegetallen = linksegetallen + 1
+            else: 
+                rechtsegetallen = rechtsegetallen - 1
+        return juiste 
